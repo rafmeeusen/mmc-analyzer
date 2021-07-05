4598266c49547a36a18b9be3194df9ce1704da4c
Raf MEEUSEN <rafael.meeusen@mil.be>
2021-07-05 14:13:46 +0200

    cleanup and progress

--- source/mmcAnalyzer.cpp
+++ source/mmcAnalyzer.cpp
@@ -47,6 +47,7 @@ U32 mmcAnalyzer::ReadAndMarkCmdBits(U8 nrOfBits)
     frame.mFlags = 0;
     frame.mEndingSampleInclusive = mCommand->GetSampleNumber();
     mResults->AddFrame( frame );
+    ReportProgress( frame.mEndingSampleInclusive );
     return data;
 }
 
@@ -112,7 +113,6 @@ void mmcAnalyzer::WorkerThread()
 
 // { Dot, ErrorDot, Square, ErrorSquare, UpArrow, DownArrow, X, ErrorX, Start, Stop, One, Zero }
     for( ; ; )
-    //for (int i=0; i<7;i++) 
     { 
 
         // find start bit:
@@ -198,51 +198,6 @@ void mmcAnalyzer::WorkerThread()
     }
 
 
-	//for( ; ; )
-	{
-		U8 data = 0;
-		U8 mask = 1 << 7;
-//		if( mCommand->GetBitState() == BIT_HIGH )
-		U64 starting_sample = mClock->GetSampleNumber();
-//		mClock->Advance( samples_to_first_center_of_first_data_bit );
-
-		for( U32 i=0; i<8; i++ )
-		{
- //          mClock->AdvanceToNextEdge();
- //          mClock->AdvanceToNextEdge(); 
-			//let's put a dot exactly where we sample this bit:
-//			if( mClock->GetBitState() == BIT_HIGH )
-//				data |= mask;
-//			mClock->Advance( samples_per_bit );
-//			mask = mask >> 1;
-		}
-
-        data = 0x22;
-		//we have a byte to save. 
-		Frame frame;
-		frame.mData1 = data;
-		frame.mFlags = 0;
-		frame.mStartingSampleInclusive = starting_sample;
-		frame.mEndingSampleInclusive = mClock->GetSampleNumber();
-//		mResults->AddFrame( frame );
-//		mResults->CommitResults();
-		//ReportProgress( frame.mEndingSampleInclusive );
-	}
-
-
-/////////////////////////////////////////////////
-////// check if these two stops are still printed:
-    for (int j=0; j<2; j++) mClock->AdvanceToNextEdge();
-    mCommand->AdvanceToAbsPosition( mClock->GetSampleNumber() ) ;
-	mResults->AddMarker( mClock->GetSampleNumber(), AnalyzerResults::Stop, mSettings->mClockChannel );
-	mResults->AddMarker( mCommand->GetSampleNumber(), AnalyzerResults::Stop, mSettings->mCommandChannel );
-    mResults->CommitResults();
-
-    for (int j=0; j<2; j++) mClock->AdvanceToNextEdge();
-    mCommand->AdvanceToAbsPosition( mClock->GetSampleNumber() ) ;
-	mResults->AddMarker( mClock->GetSampleNumber(), AnalyzerResults::Stop, mSettings->mClockChannel );
-	mResults->AddMarker( mCommand->GetSampleNumber(), AnalyzerResults::Stop, mSettings->mCommandChannel );
-    mResults->CommitResults();
 
 }
 
